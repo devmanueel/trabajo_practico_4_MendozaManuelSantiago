a03707a0c2d31744958db4e48ea85316bf582319
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-05-27 13:52:02 -0300

    agregado de main,header,seccion noticias a medias , seccion acerca de, inicio y aside

--- src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,9 +1,10 @@
 package ar.edu.unju.fi.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype
+.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
