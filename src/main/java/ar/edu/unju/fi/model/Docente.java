8a40c2302169ce0bc7d6afc206f1f5fc4ad3283c
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-05-29 14:16:30 -0300

    agregacion de navbar fijo y enlaces en la pagina ademas de agregacion de paquete model con sus respectivas clases y por ultimo video explicativo de unju; ingresante en la seccion "inicio".

---
+++ src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -0,0 +1,64 @@
+package ar.edu.unju.fi.model;
+
+public class Docente {
+	
+	private String legajo;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private int telefono;
+	
+	public Docente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Docente(String legajo, String nombre, String apellido, String email, int telefono) {
+		this.legajo = legajo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+	}
+
+	public String getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+}
---
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,114 @@
+package ar.edu.unju.fi.model;
+
+public class Materia {
+	private String codigo;
+	private String nombre;
+	private String curso;
+	private int cantidadHoras;
+	private Modalidad modalidad;
+    private Docente docente;
+    private Carrera carrera;
+    
+    public Materia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public Materia(String codigo, String nombre, String curso, int cantidadHoras, Modalidad modalidad, Docente docente,
+			Carrera carrera) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.curso = curso;
+		this.cantidadHoras = cantidadHoras;
+		this.modalidad = modalidad;
+		this.docente = docente;
+		this.carrera = carrera;
+	}
+
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getCurso() {
+		return curso;
+	}
+
+
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+
+
+	public int getCantidadHoras() {
+		return cantidadHoras;
+	}
+
+
+
+	public void setCantidadHoras(int cantidadHoras) {
+		this.cantidadHoras = cantidadHoras;
+	}
+
+
+
+	public Modalidad getModalidad() {
+		return modalidad;
+	}
+
+
+
+	public void setModalidad(Modalidad modalidad) {
+		this.modalidad = modalidad;
+	}
+
+
+
+	public Docente getDocente() {
+		return docente;
+	}
+
+
+
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}
+
+
+
+	public Carrera getCarrera() {
+		return carrera;
+	}
+
+
+
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+
+	
+}
