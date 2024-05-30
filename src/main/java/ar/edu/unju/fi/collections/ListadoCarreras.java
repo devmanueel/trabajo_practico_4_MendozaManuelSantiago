0655076b0c559d69c49b748212664b24f9b5c27e
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-05-30 19:22:57 -0300

    agregado de listado de carreras

---
+++ src/main/java/ar/edu/unju/fi/collections/ListadoCarreras.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.model.Carrera;
+
+public class ListadoCarreras {
+	public static List<Carrera> carreras = new ArrayList<Carrera>();
+	//metodo para listar
+		public static List<Carrera> listarCarreras(){
+			return carreras;
+		}
+		
+		//metodo para buscar una carrera por ID
+		
+		
+		//metodo para agregar una carrera
+		
+		//metodo para modificar un carrera 
+		
+		//metodo para elimonar una carrera
+}
